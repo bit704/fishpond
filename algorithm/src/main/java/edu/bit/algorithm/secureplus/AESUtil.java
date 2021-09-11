aa502dfeeef3919168c879193d001c727371eed1
bit704 <1623943570@qq.com>
2021-09-11 16:58:57 +0800

    fix:彻底完善加密体系

--- algorithm/src/main/java/edu/bit/algorithm/secureplus/AESUtil.java
+++ algorithm/src/main/java/edu/bit/algorithm/secureplus/AESUtil.java
@@ -1,28 +1,38 @@
 package edu.bit.algorithm.secureplus;
 
 
+import javax.crypto.*;
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class AESUtil {
 
-
-    /**
-     * 生成密钥
-     */
-    private static String generateKey() throws Exception {
-        return "";
+    public static SecretKey generateKey() throws NoSuchAlgorithmException { // 生成密钥
+        KeyGenerator secretGenerator = KeyGenerator.getInstance("AES");
+        SecureRandom secureRandom = new SecureRandom();
+        secretGenerator.init(secureRandom);
+        SecretKey secretKey = secretGenerator.generateKey();
+        return secretKey;
     }
 
-    /**
-     * 数据加密: 明文 -> 密文
-     */
-    public static String encrypt(String plaintext, String key) throws Exception {
-        return "";
+    static Charset charset = Charset.forName("UTF-8");
+
+    public static byte[] encrypt(byte[] content, SecretKey secretKey) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException { // 加密
+        return AES(content, Cipher.ENCRYPT_MODE, secretKey);
     }
 
-    /**
-     * 数据解密: 密文 -> 明文
-     */
-    public static String decrypt(byte[] ciphertext, String key) throws Exception {
-        return "";
+    public static byte[] decrypt(byte[] content, SecretKey secretKey) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException { // 解密
+        return AES(content, Cipher.DECRYPT_MODE, secretKey);
+    }
+
+    private static byte[] AES(byte[] contentArray, int mode, SecretKey secretKey)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(mode, secretKey);
+        byte[] result = cipher.doFinal(contentArray);
+        return result;
     }
 
 
